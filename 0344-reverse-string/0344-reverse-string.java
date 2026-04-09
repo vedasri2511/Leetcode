0ffed350a92b68e60df4a7aaa50d5153c7d116c4
Veda Sri <ugs23153_it.durga@cbit.org.in>
2026-04-09 21:27:50 +0530

    Time: 0 ms (100%), Space: 48 MB (90.7%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,9 +1,11 @@
 class Solution {
     public void reverseString(char[] s) {
-        for (int i = 0, j = s.length - 1; i < j; i++, j--) {
-            char t = s[i];
-            s[i] = s[j];
-            s[j] = t;
+        int i = 0, j = s.length - 1;
+        while (i < j) {
+            s[i] ^= s[j];
+            s[j] ^= s[i];
+            s[i] ^= s[j];
+            i++; j--;
         }
     }
 }
