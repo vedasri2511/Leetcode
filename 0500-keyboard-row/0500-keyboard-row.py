e8dddb579f782127380b25448d825b37423f931b
Veda Sri <ugs23153_it.durga@cbit.org.in>
2026-05-10 21:47:06 +0530

    Time: 0 ms (100%), Space: 19.4 MB (28.74%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findWords(self, words):
+        rows = [
+            set("qwertyuiop"),
+            set("asdfghjkl"),
+            set("zxcvbnm")
+        ]
+
+        return [
+            w for w in words
+            if any(set(w.lower()) <= row for row in rows)
+        ]
