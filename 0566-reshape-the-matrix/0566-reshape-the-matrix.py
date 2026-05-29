17e136ecf8677f421cb539f940e1bb4313ac24be
Veda Sri <ugs23153_it.durga@cbit.org.in>
2026-05-29 22:23:14 +0530

    Time: 4 ms (11.72%), Space: 20 MB (30.74%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def matrixReshape(self, mat, r, c):
+        m, n = len(mat), len(mat[0])
+
+        if m * n != r * c:
+            return mat
+
+        res = [[0] * c for _ in range(r)]
+
+        for i in range(m * n):
+            res[i // c][i % c] = mat[i // n][i % n]
+
+        return res
